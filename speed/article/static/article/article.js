074686f10baec54fe99926526beef9cc78827df4
Your Name <you@example.com>
2020-08-22 20:17:29 +0300

    tmp

--- speed/article/static/article/article.js
+++ speed/article/static/article/article.js
@@ -1,1 +1,23 @@
-alert("hi");
+$( document ).ready(function() {
+    function show_result(comments){
+        var $comments_div = $("#comment");
+        $comments_div.html(comments);
+    }
+
+    function request_comments(){
+        $.ajax({
+          method: "GET",
+          url: "/comment/",
+        })
+          .done(function( comments ) {
+            show_result(comments);
+          });
+    }
+
+    function add_handlers(){
+        $("#comment-btn").click(function(){
+            request_comments();
+        });
+    }
+    add_handlers();
+});
